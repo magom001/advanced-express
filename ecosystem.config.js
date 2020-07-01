0fbeccb9084aec6d1a85eb0c9f1720e4ae69d347
magom001 <magom001@yandex-team.ru>
2020-07-01 18:27:45 +0300

    pm2 sample config

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+// Config file for PM2
+module.exports = {
+  apps: [
+    {
+      name: "meetup",
+      script: "bin/www",
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: "nodejs",
+      host: "<CHANGE_TO_YOUR_HOST>",
+      ref: "origin/master",
+      repo: "<CHANGE_TO_YOUR_GITHUB_REPO>",
+
+      // Make sure this directory exists on your server or change this entry to match your directory structure
+      path: "/home/nodejs/deploy",
+
+      "post-deploy":
+        "cp ../.env ./ && npm install && pm2 startOrRestart ecosystem.config.js --env production",
+    },
+  },
+};
